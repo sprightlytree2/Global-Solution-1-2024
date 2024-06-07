e8b6a6ce5f9c07370ab48d0296fdadd86f66f7d8
sprightlytree2 <luccasdaccosta@gmail.com>
2024-06-07 10:08:17 -0300

    Retirando imports desnecessarios

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,4 @@
-import { Router, request, response, Request, Response } from "express";
+import { Router, Request, Response } from "express";
 
 import { getPosts, postPost } from "./controller/PostController";
 
